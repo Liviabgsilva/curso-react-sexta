b87882ad534bda6d7930ac2d6402597da1d4839d
Liviabgsilva <livia_bg_silva@estudante.sesisenai.org.br>
2026-07-17 15:55:31 -0300

    novo

--- 01-estados/src/Components/Peba.jsx
+++ 01-estados/src/Components/Peba.jsx
@@ -1,18 +1,21 @@
-import{useState} from "react"
+import { useState } from "react";
 function Peba() {
-    const[resultado, setResultado] =useState()
+    const[resultado, setResultado] = useState()
+    
     function calcularPontos(){
-let vitorias = Number(prompt("Vitórias:"))
-let empates = Number(prompt("Empates:"))
-let pontos = vitorias*3 + empates
-setResultado(pontos)
+        let vitorias = Number(prompt("Vitórias:"))
+        let empates = Number(prompt("Empates:"))
+        let pontos = vitorias*3 + empates
+        setResultado(pontos); 
     }
   return (
     <div className="cont-peba">
-        <h2> Peba e o campeonato</h2>
-        <button onClick={calcularPontos}>Calcular pontos</button>
-        pontos: {resultado}
-        </div>
+        <h2>Peba e o campeonato</h2>
+        <button onClick={calcularPontos}>Calcular os pontos</button>
+        {resultado && <p>Pontos: {resultado}</p>}
+        {/* <p>Pontos: {resultado}</p> */}
+        {resultado == 10 && <p>Estou renderizado</p>}
+    </div>
   )
 }
 
--- 01-estados/src/Components/Pepequeno.jsx
+++ 01-estados/src/Components/Pepequeno.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
 
 function PePequeno() {
-  const [valeTroca, setValeTroca] = useState(0);
-  function calcularTroca() {
-    let precoPar = Number(prompt("Digite o preço de cada par de calçado (R$):"));
-    let qtdTrocados = Number(prompt("Digite a quantidade de pares trocados:"));
-
-    let totalVale = precoPar * qtdTrocados;
-
-    setValeTroca(totalVale);
-  }
-
   return (
-    <div className="cont-pepequeno">
-      {}
-      <h2>Pé Pequeno - Cálculo de Vale Trocas</h2>
-      <button onClick={calcularTroca}>Calcular Vale Troca</button>
-      <p>Valor do Vale Trocas: R$ {valeTroca.toFixed(2)}</p>
+    <div className='cont-pe-pequeno'>
+        <h2>Pé Pequeno e as trocas</h2>
     </div>
-  );
+  )
 }
 
-export default PePequeno;
+export default PePequeno
